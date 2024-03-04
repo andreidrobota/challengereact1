303b53a87bf0c69ecba76f0db7bf9fad3749e5df
andreidrobota <drobotaandrei@gmail.com>
2024-03-04 15:21:11 +0000

    Style sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react"
+import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
-    return <div> <a href="#">My Photos</a><a href="#">My Illustrations</a><a href="#">My Paintings</a> </div>
+    return <div className={css.sidebar}> <a href="#">My Photos</a><a href="#">My Illustrations</a><a href="#">My Paintings</a> </div>
 }
 
 export default Sidebar;
